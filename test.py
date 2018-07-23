6b7cc53ebfc288fe3e336ae2a69b4e2a0c89d454
Qian Huang <cianhuang@outlook.com>
2018-07-23 22:24:01 +0800

    learn

--- test.py
+++ test.py
@@ -37,47 +37,69 @@
 #     optimizer.step()
 
 
-#-----------predict curve------------#
+# #-----------predict curve------------#
+# import torch
+# import torch.nn.functional as F
+# import matplotlib.pyplot as plt
+# from torch.autograd import Variable
+
+# class Net(torch.nn.Module):
+#     def __init__(self, n_feature, n_hidden, n_output):
+#         super(Net, self).__init__()
+#         self.hidden = torch.nn.Linear(n_feature, n_hidden)
+#         self.predict =torch.nn.Linear(n_hidden, n_output)
+
+#     def forward(self, x):
+#         x = F.relu(self.hidden(x))
+#         x = self.predict(x)
+#         return x
+
+# net = Net(n_feature=1, n_hidden=10, n_output=1)
+
+# x = torch.unsqueeze(torch.linspace(-1, 1, 100), dim=1)
+# y = x.pow(2) + 0.2*torch.rand(x.size())
+
+# x, y = Variable(x), Variable(y)
+
+# optimizer = torch.optim.SGD(net.parameters(), lr = 0.5)
+# loss_func = torch.nn.MSELoss()
+
+# plt.ion()   # 画图
+# plt.show()
+
+# for t in range(100):
+#     prediction = prediction = net(x)
+#     loss = loss_func(prediction, y)
+
+#     optimizer.zero_grad()
+#     loss.backward()
+#     optimizer.step()
+
+#     if t % 5 == 0:
+#         # plot and show learning process
+#         plt.cla()
+#         plt.scatter(x.data.numpy(), y.data.numpy())
+#         plt.plot(x.data.numpy(), prediction.data.numpy(), lw=5)
+#         plt.pause(0.1)
+
 import torch
-import torch.nn.functional as F
-import matplotlib.pyplot as plt
-from torch.autograd import Variable
+import torch.utils.data as Data
 
-class Net(torch.nn.Module):
-    def __init__(self, n_feature, n_hidden, n_output):
-        super(Net, self).__init__()
-        self.hidden = torch.nn.Linear(n_feature, n_hidden)
-        self.predict =torch.nn.Linear(n_hidden, n_output)
+BATCH_SIZE = 8
 
-    def forward(self, x):
-        x = F.relu(self.hidden(x))
-        x = self.predict(x)
-        return x
+x = torch.linspace(1, 10, 10)
+y = torch.linspace(10, 1, 10)
 
-net = Net(n_feature=1, n_hidden=10, n_output=1)
+torch_dataset = Data.TensorDataset(x, y)
+loader = Data.DataLoader(
+    dataset=torch_dataset,
+    batch_size=BATCH_SIZE,
+    shuffle=True,
+    num_workers=2,
+)
 
-x = torch.unsqueeze(torch.linspace(-1, 1, 100), dim=1)
-y = x.pow(2) + 0.2*torch.rand(x.size())
+for epoch in range(3):
+    for step, (batch_x, batch_y) in enumerate(loader):
 
-x, y = Variable(x), Variable(y)
-
-optimizer = torch.optim.SGD(net.parameters(), lr = 0.5)
-loss_func = torch.nn.MSELoss()
-
-plt.ion()   # 画图
-plt.show()
-
-for t in range(100):
-    prediction = prediction = net(x)
-    loss = loss_func(prediction, y)
-
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
-
-    if t % 5 == 0:
-        # plot and show learning process
-        plt.cla()
-        plt.scatter(x.data.numpy(), y.data.numpy())
-        plt.plot(x.data.numpy(), prediction.data.numpy(), lw=5)
-        plt.pause(0.1)
+        print('Epoch: ', epoch, '| Step: ', step, '| batch x: ',
+              batch_x.numpy(), '| batch y: ', batch_y.numpy())
