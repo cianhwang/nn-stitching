b91ab192e60fc9f369e47d86d728651ca74164f1
Qian Huang <cianhuang@outlook.com>
2018-07-26 14:08:48 +0800

    update 7-26

--- patch-match.py
+++ patch-match.py
@@ -26,12 +26,22 @@
 import tensorflow as tf
 import numpy as np
 
+def fun_zeroPadding(M, fringe):
+    m, n, band = M.shape
+    M_padding = np.zeros(m+fringe*2, n+fringe*2, band)
+    M_padding[1:-1, 1:-1, :] = M
+    return M_padding
+
+def fun_patchCrop(M, leftupperX, leftupperY, patchSize): #
+    patch = M[leftupperX:(leftupperX+patchSize-1), leftupperY:(leftupperY+patchSize-1), :]
+    return patch
+
 def fun_patchSample(M, Stride = 1):
     m, n, band = M.shape
-    assert (m==n and band==3)
+    assert m==n
     patchSize = 3
     patchStack = np.zeros([m, n, band, m*n])
-    M_padding = tf.image.pad_to_bounding(M, 1, 1, m+2, n+2)
+    M_padding = fun_zeroPadding(M, (patchSize-1)/2)
     for i in range(m):
         for j in range(n):
             singlePatch = fun_patchCrop(
@@ -40,12 +50,9 @@ def fun_patchSample(M, Stride = 1):
             patchStack[:,:,:,i*m+j] = singlePatch
     return patchStack
 
-def fun_patchCrop():
-    pass
-
 def fun_patchConv(M_LR, patchStack):
     m1, n1, band1 = M_LR.shape
-    m2, n2, band2 = patchStack.shape
+    m2, n2, band2, dim = patchStack.shape # m2: patch size
     assert band1 == band2
     # !!-----conv: use similarity score.-----
     # tf_patchStack = tf.constant(patchStack, tf.float32)
@@ -53,17 +60,24 @@ def fun_patchConv(M_LR, patchStack):
     # scoreMap = tf.nn.conv2d(tf_M_LR, tf_patchStack, strides = [1,1,1,1], padding='SAME')
     # with tf.Session() as sess:
     #     scoreMap = sess.run(scoreMap)
+    M_LR_zeroPadding = fun_zeroPadding(M_LR, (m2-1)/2)
+    scoreMap = np.zeros([m1, n1])
+    maxIdxMap = np.zeros([m1, n1])
+    for i in range(m1):
+        for j in range(n1):
+            for k in range(dim): # optimize...
+                patch_M_LR = fun_patchCrop(M_LR, i, j, m2)
+                simScore = fun_simScore(patch, patch_M_LR)
+                if simScore > scoreMap[i, j]:
+                    maxIdxMap[i, j] = k
+                    scoreMap[i, j] = simScore
+    return maxIdxMap
 
-    return scoreMap
 
 def fun_simScore(patch_LR, patch_LRef):
     vec_LR = patch_LR.reshape([-1, 1])
     vec_LRef = patch_LRef.reshape([-1, 1])
     score = np.dot(vec_LR.T, vec_LRef)/(np.linalg.norm(vec_LR)*np.linalg.norm(vec_LRef))
-
-def fun_locateCorr(scoreMap):
-    maxMap = np.amax(scoreMap, axis=2) #np.amax
-    maxIdxMap = np.where(scoreMap==maxMap) # !!need refine.
 
 def fun_combine(M_s, M_Ref):
     m, n, band = M_s.shape()
