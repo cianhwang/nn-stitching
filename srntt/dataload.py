138c6f9265f3517a85d6f39f3aa29c974e8f94a2
Qian Huang <cianhuang@outlook.com>
2018-08-04 16:14:38 +0800

    updt

--- srntt/dataload.py
+++ srntt/dataload.py
@@ -1,11 +1,10 @@
 import numpy as np
 import h5py
 
-def dataLoader(data_file, dataNum):
-        assert dataNum > 0
+def dataLoader(data_file):
         file=h5py.File(str(data_file),'r')        
         ref=file['ref'][:].astype(np.float32)
         label=file['label'][:].astype(np.float32)
         ref=ref.transpose(0,3,2,1)
         label=np.transpose(label,(0,3,2,1))
-        return ref[:dataNum, :, :, :]/255.0, label[:dataNum, :, :, :]/255.0
+        return ref/255.0, label/255.0
